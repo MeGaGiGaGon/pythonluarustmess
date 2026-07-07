15fefe4b98d58daed693e18d3e48186cbdac3fd8
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2026-07-07 11:42:40 -0700

    Figured out forward ref

--- interpreters/cst_ast_idea.py
+++ interpreters/cst_ast_idea.py
@@ -105,6 +105,27 @@ class SimpleParser[I, O](Parser[I, O, Literal[False]]):
         return SimpleParser(func)
 
 
+class ForwardRefParser[I, O](Parser[I, O, Literal[False]]):
+    def __init__(self, func: Callable[[], ParserFunc[I, O]]) -> None:
+        self._func: Callable[[], ParserFunc[I, O]] = func
+
+    @overload
+    def predicate(self: Parser[I, O, Literal[True]]) -> Sequence[Sequence[I]]: ...
+    @overload
+    def predicate(self: Parser[I, O, Literal[False]]) -> None: ...
+    @override
+    def predicate(self) -> Sequence[Sequence[I]] | None:
+        return None
+
+    @override
+    def func(self) -> ParserFunc[I, O]:
+        return self._func()
+
+    @override
+    def with_new_func[U](self, func: ParserFunc[I, U]) -> SimpleParser[I, U]:
+        return SimpleParser(func)
+
+
 def chain[I, O](*parsers: Parser[I, O, Literal[True]]) -> Parser[I, O, Literal[True]]:
     new_predicates: list[Sequence[I]] = []
     for parser in parsers:
@@ -147,27 +168,19 @@ def just_seq[I](seq: Sequence[I]) -> PredicateParser[I, Sequence[I]]:
                 raise ValueError(msg)
             seq_index += 1
             index += 1
-        if seq_index <= len(seq):
+        if seq_index < len(seq):
             msg = f"Expected {seq[seq_index]!r} (part of {seq=!r}), but input ran out at {index=}"
             raise ValueError(msg)
         return seq, index
 
     return PredicateParser(inner, [seq])
 
-@overload
-def to_simple_parser[I, O](func: ParserFunc[I, O], /) -> Parser[I, O, Literal[False]]: ...
-@overload
-def to_simple_parser[I, O](predicates: Sequence[Sequence[I]], /) -> Callable[[ParserFunc[I, O]], Parser[I, O, Literal[True]]]: ...
-def to_simple_parser[I, O](args: ParserFunc[I, O] | Sequence[Sequence[I]], /) -> Callable[[ParserFunc[I, O]], Parser[I, O, Literal[True]]] | Parser[I, O, Literal[False]]:
-    if isinstance(args, Sequence):
-        def inner(func: ParserFunc[I, O]) -> Parser[I, O, Literal[True]]:
-            return PredicateParser(func, args)
-        return inner
-    else:
-        return SimpleParser(args)
+foo = chain(
+    just_seq("a").then(ForwardRefParser(lambda: foo)).map(lambda x: [*x[0], *x[1]]),
+    just_seq("b")
+)
 
-@to_simple_parser([["a"]])
-def foo(x: Sequence[str], y: int) -> ParserResult[int]:
-    return chain(foo.then("b"))(x, y)
-
-foo("a", 0)
+print(foo("b", 0))
+print(foo("ab", 0))
+print(foo("aab", 0))
+print(foo("aaab", 0))
